64014353262e0d14a4950711579ad15ce6d22cdc
Jhonathan Pérez Ortega <jperez_ortega@hotmail.com>
2021-03-26 17:31:40 -0500

    Programa configuración inicial del servidor

---
+++ src/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+
+//Server configuration
+app.set('port', 4000);
+app.set('json spaces', 2);
+
+
+//Server middlewares
+app.use(morgan('dev'));
+app.use(express.json());
+
+
+app.listen(app.get('port'), (err) => {
+	err ? console.log("We got an error: ", err) : console.log(`App is running on port ${app.get('port')}`);
+})
