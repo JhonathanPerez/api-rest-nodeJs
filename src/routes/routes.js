228e197ba0012f558f3b13f42b76919929f62d1e
Jhonathan Pérez Ortega <jperez_ortega@hotmail.com>
2021-03-29 12:02:02 -0500

    Crea metodos auxiliares para realizar operaciones CRUD

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,5 +1,81 @@
 const { Router } = require('express');
 const router = Router();
+const { Users } = require('../../models');
+
+
+const findAllUsers = async () => {
+	try {
+		return await Users.findAll();
+	} catch (err) {
+		console.log("We got an error: ", err);
+	}
+}
+
+const findUserByEmail = async (email) => {
+	try {
+		return await Users.findAll({
+			where: {
+				email: email
+			}
+		});
+	} catch (err) {
+		console.log("We got an error: ", err);
+	}
+}
+
+const findUserById = async (id) => {
+	try {
+		return await Users.findAll({
+			where: {
+				id: id
+			}
+		});
+	} catch (err) {
+		console.log("We got an error: ", err);
+	}
+}
+
+const createUser = async (firstName, lastName, email) => {
+	try {
+		return await Users.create({
+			firstName: firstName,
+			lastName: lastName,
+			email: email
+		});
+	} catch (err) {
+		console.log("We got an error: ", err);
+	}
+}
+
+const updateUser = async (firstName, lastName, email, id) => {
+	try {
+		await Users.update({
+			firstName: firstName,
+			lastName: lastName,
+			email: email
+		},
+			{
+				where: {
+					id: id
+				}
+			});
+	} catch (err) {
+		console.log("We got an error on update: ", err);
+	}
+}
+
+const deleteUser = async (id) => {
+	try {
+		await Users.destroy({
+			where: {
+				id: id
+			}
+		});
+	} catch (err) {
+		console.log(err);
+	}
+}
+
 
 router.get('/', (req, res) => {
 	res.send();
