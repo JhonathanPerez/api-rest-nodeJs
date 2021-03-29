da96b86fd78c3fcf5538a3490421b0f449a313f2
Jhonathan Pérez Ortega <jperez_ortega@hotmail.com>
2021-03-29 12:04:38 -0500

    Crea función auxiliar para validar email

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -76,6 +76,10 @@ const deleteUser = async (id) => {
 	}
 }
 
+function validateEmail(email) {
+	const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+	return re.test(String(email).toLowerCase());
+}
 
 router.get('/', (req, res) => {
 	res.send();
